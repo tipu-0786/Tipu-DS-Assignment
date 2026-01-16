fd0339c8a45c814de13bc83805dad940bd2d6e49
tipu-0786 <md.25168@ee.du.ac.in>
2026-01-16 10:03:39 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+"""
+Algorithm to Arrange Clothes in an Almirah
+
+1. Start
+2. Take input as number of shelves (n) in the almirah.
+3. Identify different categories of clothes such as:
+   - Formal wear
+   - Casual wear
+   - Traditional wear
+   - Daily wear
+4. Assign one shelf to each category based on frequency of use.
+5. Place daily and frequently used clothes on middle shelves
+   so that they can be accessed easily.
+6. Place less frequently used clothes on top or bottom shelves.
+7. Fold clothes properly to save space.
+8. Arrange clothes in stacks according to type and size.
+9. Label shelves if required to reduce search time.
+10. End
+"""
